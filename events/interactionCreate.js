a62198edb7fd390ba982036963b1d3e64f3c2b77
light2k4 <augustin.pro.prs@gmail.com>
2024-07-29 21:54:37 +0200

    chore: Add error handling for guild-only command in interactionCreate event handler

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 // MPBot by light2k4 (https://discord.gg/YmA88jc7GF)
 // Cet event permet de gérer les interactions du bot.
 
-const { Events } = require("discord.js");
+const { Events, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
 const log = require("../logger");
 const db = require("../db");
 
@@ -15,6 +15,19 @@ module.exports = {
             if (!command) {
                 log.error(`❌ Command ${interaction.commandName} not found`);
                 return interaction.reply({ content: 'This command is not available.', ephemeral: true });
+            } else {
+                if (!interaction.guild) {
+                    const errorEmbed = new EmbedBuilder()
+                        .setTitle('303 - Use in guild')
+                        .setColor('#FF0000')
+                        .setDescription('This command can only be used in a guild.');
+                    const button = new ButtonBuilder()
+                        .setLabel('Invite me')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL(process.env.INVITE_URL);
+                    const actionRow = new ActionRowBuilder().addComponents(button);
+                    return await interaction.reply({ embeds: [errorEmbed], components: [actionRow] });
+                }
             }
 
             await command.execute(interaction);
