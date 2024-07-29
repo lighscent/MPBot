8116b02ba6a5ef6d4e9d4cd7b1c47162c68446c5
light2k4 <augustin.pro.prs@gmail.com>
2024-07-29 17:30:19 +0200

    chore: first commit

---
+++ client.js
@@ -0,0 +1,13 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
+    ]
+})
+
+client.login(process.env.TOKEN);
